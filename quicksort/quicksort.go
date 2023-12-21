2445db9caf933f59d47e4a3924fc7d4b001a59fa
Matt-VolScuo <129525952+Matt-VolScuo@users.noreply.github.com>
2023-12-21 10:04:37 +0100

    Update quicksort.go

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -23,3 +23,22 @@ func Quicksort(liste []int) {
 	copy(liste, liste1)
 
 }
+
+func Partition(liste []int) ([]int, []int) {
+	if len(liste) <= 1 {
+		return liste, nil
+	}
+	pivot := liste[0]
+
+	var liste1, liste2 []int
+
+	for i := 1; i < len(liste); i++ {
+		if liste[i] < pivot {
+			liste1 = append(liste1, liste[i])
+		} else {
+			liste2 = append(liste2, liste[i])
+		}
+	}
+
+	return liste1, liste2
+}
