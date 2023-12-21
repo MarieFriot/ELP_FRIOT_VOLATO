1e4de9423d94f2514eab379e182896c47ffb3774
Matt-VolScuo <129525952+Matt-VolScuo@users.noreply.github.com>
2023-12-21 09:11:38 +0100

    Create quicksort.go

---
+++ quicksort/quicksort.go
@@ -0,0 +1,25 @@
+package quicksort
+
+func Quicksort(liste []int) {
+	if len(liste) <= 1 {
+		return
+	}
+	pivot := liste[0]
+
+	var liste1, liste2 []int
+
+	for i := 1; i < len(liste); i++ {
+		if liste[i] < pivot {
+			liste1 = append(liste1, liste[i])
+		} else {
+			liste2 = append(liste2, liste[i])
+		}
+	}
+	Quicksort(liste1)
+	Quicksort(liste2)
+
+	liste1 = append(liste1, pivot)
+	liste1 = append(liste1, liste2...) //... décompose les elements individuels de liste2 et les ajoute à liste1
+	copy(liste, liste1)
+
+}
