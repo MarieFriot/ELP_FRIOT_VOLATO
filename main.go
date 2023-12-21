a1941719c8f5122698abfa65e8328a959f51c7a2
Matt-VolScuo <129525952+Matt-VolScuo@users.noreply.github.com>
2023-12-21 08:30:40 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main 
+
+import (
+  "fmt"
+  "quicksort" //notre bibliotheque avec la fonction quicksort dedans
+)
+
+func main() {
+  liste := []int{1,5,9,8,3,6,4,7,2}
+  quicksort(liste)
+  fmt.Println(liste)
+}
