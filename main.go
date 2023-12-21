fa6985750ccf8d59fac1b55f5e01d90bb132a2cd
Matt-VolScuo <129525952+Matt-VolScuo@users.noreply.github.com>
2023-12-21 10:40:27 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,36 @@ import (
   "sync"
 )
 
-func main() {
-  liste := []int{1,5,9,8,3,6,4,7,2}
-  quicksort.Quicksort(liste)
-  fmt.Println(liste)
+//Parallel quicksort version 2 processeur
+func ParallelQSv2(data []int, wg *sync.WaitGroup, channel chan []int) {
+  defer wg.Done()
+
+  if len(data) <= 1 {
+    ch <- data
+    return
+  }
+
+  var low, high []int
+  low, high := quicksort.Partition(data)
+
+  lowerWg := &sync.WaitGroup{}
+  upperWg := &sync.WaitGroup{}
+  lowerCh := make(chan []int)
+  upperCh := make(chan []int)
+
+  lowerWg.Add(1)
+  go ParallelQSv2(low, lowerWg, lowerCh)
+
+  upperWg.Add(1)
+  go ParallelQSv2(high, upperWg, upperCh)
+
+  lowerWg.Wait()
+  upperWg.Wait()
+
+  result := append(<- lowerCh, pivot)
+  result = append(result, <- upperCh)
+
+  ch <- result
+  }
+  
 }
