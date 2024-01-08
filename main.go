4b8619724ddf99cd8530aa42843ad2e592018188
MarieFriot <154432198+MarieFriot@users.noreply.github.com>
2024-01-08 17:43:10 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
-	"ELP_FRIOT_VOLATO-Golang/quicksort"
+	"test_quicksort_para/quicksort"
 	"time"
 )
-
-func splitList(liste []int) ([]int, []int) {
-	milieu := len(liste) / 2
-	return liste[:milieu], liste[milieu:]
-}
 
 func bigListeGen(size int) []int {
 	liste := []int{}
@@ -21,56 +15,17 @@ func bigListeGen(size int) []int {
 	return liste
 }
 
-func QuicksortParallel(liste []int) []int {
-	var wg sync.WaitGroup
-
-	if len(liste) <= 1 {
-		return liste
-	}
-	l1, l2 := splitList(liste)
-	pivot := liste[0]
-
-	l1Ch := make(chan struct{ l1Low, l1Up []int })
-	l2Ch := make(chan struct{ l2Low, l2Up []int })
-
-	wg.Add(2)
-	go func() {
-		l1Low, l1Up := quicksort.Partition(l1, pivot, &wg)
-		l1Ch <- struct{ l1Low, l1Up []int }{l1Low, l1Up}
-	}()
-	go func() {
-		l2Low, l2Up := quicksort.Partition(l2, pivot, &wg)
-		l2Ch <- struct{ l2Low, l2Up []int }{l2Low, l2Up}
-	}()
-	wg.Wait()
-
-	result1 := <-l1Ch
-	result2 := <-l2Ch
-
-	lLow := append(result1.l1Low, result2.l2Low...)
-	lUp := append(result1.l1Up, result2.l2Up...)
-
-	wg.Add(2)
-	go quicksort.Quicksort(lLow, &wg)
-	go quicksort.Quicksort(lUp, &wg)
-	wg.Wait()
-
-	lLow = append(lLow, lUp...)
-	return lLow
-
-}
-
 func main() {
 	liste3 := bigListeGen(100000)
 	liste4 := make([]int, len(liste3))
 	copy(liste4, liste3)
 
 	startTime := time.Now()
-	result := QuicksortParallel(liste3)
+	result := quicksort.QuicksortParallel(liste3)
 	endTime := time.Now()
 	duration := endTime.Sub(startTime)
 	fmt.Printf("Temps d'exécution parralllele : %v\n", duration)
-	fmt.Println(result[0])
+	fmt.Printf("%v\n", result[0])
 
 	startTime = time.Now()
 	quicksort.QuicksortSeq(liste4)
