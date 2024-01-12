f72e3e479d80426d58d9a660971e249e8b77e30a
MarieFriot <154432198+MarieFriot@users.noreply.github.com>
2024-01-12 16:58:19 +0100

    Add files via upload

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -1,22 +1,24 @@
 package quicksort
 
 import (
-	"fmt"
 	"sync"
 )
 
 func splitList(liste []int, listNumber int) [][]int {
 	res := make([][]int, listNumber)
-	separator := []int{}
-	for i := 1; i < listNumber; i++ {
-		separator = append(separator, len(liste)/listNumber*i)
+	if listNumber == 1 {
+		res[0] = liste[:]
+	} else {
+		separator := []int{}
+		for i := 1; i < listNumber; i++ {
+			separator = append(separator, len(liste)/listNumber*i)
+		}
+		res[0] = liste[:separator[0]]
+		for i := 1; i < listNumber-1; i++ {
+			res[i] = liste[separator[i-1]:separator[i]]
+		}
+		res[listNumber-1] = liste[separator[listNumber-2]:]
 	}
-	fmt.Println(separator)
-	res[0] = liste[:separator[0]]
-	for i := 1; i < listNumber-1; i++ {
-		res[i] = liste[separator[i-1]:separator[i]]
-	}
-	res[listNumber-1] = liste[separator[listNumber-2]:]
 	return res
 }
 
@@ -37,11 +39,12 @@ func Quicksort(liste []int, wg *sync.WaitGroup) {
 		}
 	}
 
-	var wgInner sync.WaitGroup
-	wgInner.Add(2)
-	go Quicksort(liste1, &wgInner)
-	go Quicksort(liste2, &wgInner)
-	wgInner.Wait()
+	//Ou faire des appels récursifs de Quicksort avec un waitgroup
+	//var wgInner sync.WaitGroup
+	//wgInner.Add(2)
+	QuicksortSeq(liste1)
+	QuicksortSeq(liste2)
+	//wgInner.Wait()
 
 	liste1 = append(liste1, pivot)
 	liste1 = append(liste1, liste2...) //... décompose les elements individuels de liste2 et les ajoute à liste1
@@ -51,10 +54,12 @@ func Quicksort(liste []int, wg *sync.WaitGroup) {
 
 func QuicksortParallel(liste []int, listNumber int) []int {
 	var wg sync.WaitGroup
+	var wg2 sync.WaitGroup
 
 	if len(liste) <= 1 {
 		return liste
 	}
+	print("hi")
 	tab := splitList(liste, listNumber)
 	pivot := liste[0]
 
@@ -64,6 +69,7 @@ func QuicksortParallel(liste []int, listNumber int) []int {
 	lLow := []int{}
 	lUp := []int{}
 
+	wg2.Add(1)
 	go func() {
 		for i := 0; i < listNumber; i++ {
 			l1 := <-lowCh
@@ -71,15 +77,16 @@ func QuicksortParallel(liste []int, listNumber int) []int {
 			lLow = append(lLow, l1...)
 			lUp = append(lUp, l2...)
 		}
+		defer wg2.Done()
 	}()
 
-	wg.Add(listNumber)
 	for i := 0; i < listNumber; i++ {
+		wg.Add(1)
 		go Partition(tab[i], pivot, &wg, lowCh, upCh)
 	}
 
 	wg.Wait()
-
+	wg2.Wait()
 	wg.Add(2)
 	go Quicksort(lLow, &wg)
 	go Quicksort(lUp, &wg)
@@ -106,6 +113,7 @@ func QuicksortSeq(liste []int) {
 			liste2 = append(liste2, liste[i])
 		}
 	}
+
 	QuicksortSeq(liste1)
 	QuicksortSeq(liste2)
 	liste1 = append(liste1, pivot)
@@ -116,7 +124,6 @@ func QuicksortSeq(liste []int) {
 
 func Partition(liste []int, pivot int, wg *sync.WaitGroup, chLow chan []int, chUp chan []int) {
 	defer wg.Done()
-	print("start")
 	var liste1, liste2 []int
 	for i := 0; i < len(liste); i++ {
 		if liste[i] <= pivot {
@@ -127,6 +134,5 @@ func Partition(liste []int, pivot int, wg *sync.WaitGroup, chLow chan []int, chU
 	}
 	chLow <- liste1
 	chUp <- liste2
-	print("done")
 
 }
