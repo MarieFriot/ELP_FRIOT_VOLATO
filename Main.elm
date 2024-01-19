7da110d3f67163a31701ced1d2eb16e45cdfb91f
Matt-VolScuo <129525952+Matt-VolScuo@users.noreply.github.com>
2024-01-19 09:22:16 +0100

    Update Main.elm

--- Main.elm
+++ Main.elm
@@ -1,7 +1,9 @@
-module Main exposing (..)
 import Browser
-import Html exposing (Html, text, pre)
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (..)
 import Http
+import Json.Decode exposing (Decoder, map4, field, int, string)
 
 
 
@@ -20,30 +22,39 @@ main =
 
 -- MODEL
 
+type alias Model =
+  { content : String
+  }
 
-type Model
-  = Failure
-  | Loading
-  | Success String
+init : Model
+init =
+  { content = "" }
 
+
+type alias Word =
+  { content : String
+  }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Loading
-  , Http.get
-      { url = "dico.txt"
-      , expect = Http.expectString GotText
-      }
-  )
+  (Loading, randomWord)
 
 
 
 -- UPDATE
 
+type Input
+  = Change String
+
+
+update : Input -> Model -> Model
+update Input model =
+  case Input of
+    Change newContent ->
+      { model | content = newContent }
 
 type Msg
   = GotText (Result Http.Error String)
-
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -57,9 +68,7 @@ update msg model =
           (Failure, Cmd.none)
 
 
-
 -- SUBSCRIPTIONS
-
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -72,12 +81,30 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  case model of
-    Failure ->
-      text "I was unable to load your book."
+  div []
+    [ h2 [] [ text "Guess it !" ]
+    , viewInput model
+    ]
 
-    Loading ->
-      text "Loading..."
 
-    Success fullText ->
-      pre [] [ text fullText ]
+viewInput : Model -> Html Msg
+viewInput model =
+  div []
+    [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
+    , div [] [ text (String.reverse model.content) ]
+    ]
+
+
+
+-- HTTP
+
+
+getWord : Cmd Msg
+getWord =
+  Http.get
+    { url = "dico.txt"
+    , expect = Http.expectString GotText randomWord
+    }
+
+
+randomWord :
