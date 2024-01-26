093187f4114188144752d70e80a764965f613b07
Matt-VolScuo <129525952+Matt-VolScuo@users.noreply.github.com>
2024-01-26 11:00:37 +0100

    Update LetterPileMaker.js

--- LetterPileMaker.js
+++ LetterPileMaker.js
@@ -1,7 +1,7 @@
-let Apparitions = [(14, "A"),(4,"B"),(7,"C"),(5,"D"),(19,"E"),(2,"F"),(4,"G"),
-                   (2, "H"),(11,"I"),(1,"J"),(1,"K"),(6,"L"),(5,"M"),(9,"N"),
-                   (8, "O"),(4,"P"),(1,"Q"),(10,"R"),(7,"S"),(9,"T"),(8,"U"),
-                   (2,"V"), (1,"W"), (1,"X"), (1,"Y"), (2, "Z")]
+let Apparitions = [[14, "A"],[4,"B"],[7,"C"],[5,"D"],[19,"E"],[2,"F"],[4,"G"],
+                   [2, "H"],[11,"I"],[1,"J"],[1,"K"],[6,"L"],[5,"M"],[9,"N"],
+                   [8, "O"],[4,"P"],[1,"Q"],[10,"R"],[7,"S"],[9,"T"],[8,"U"],
+                   [2,"V"], [1,"W"], [1,"X"], [1,"Y"], [2, "Z"]]
 Letters = []
 
 for (let i = 0; i < Apparitions.length; i++) {
@@ -13,4 +13,8 @@ for (let i = 0; i < Apparitions.length; i++) {
     }
 }
 
+const randomItem = arr => arr.splice((Math.random() * arr.length) | 0, 1);
+
 console.log(Letters.length + Letters)
+
+console.log(randomItem(Letters))
