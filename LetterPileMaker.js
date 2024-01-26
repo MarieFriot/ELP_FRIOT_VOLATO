5fca94cffc4182b6636e7fd6ac1129f77ab33f12
Matt-VolScuo <129525952+Matt-VolScuo@users.noreply.github.com>
2024-01-26 09:40:06 +0100

    Add files via upload

---
+++ LetterPileMaker.js
@@ -0,0 +1,16 @@
+let Apparitions = [(14, "A"),(4,"B"),(7,"C"),(5,"D"),(19,"E"),(2,"F"),(4,"G"),
+                   (2, "H"),(11,"I"),(1,"J"),(1,"K"),(6,"L"),(5,"M"),(9,"N"),
+                   (8, "O"),(4,"P"),(1,"Q"),(10,"R"),(7,"S"),(9,"T"),(8,"U"),
+                   (2,"V"), (1,"W"), (1,"X"), (1,"Y"), (2, "Z")]
+Letters = []
+
+for (let i = 0; i < Apparitions.length; i++) {
+    let amount= Apparitions[i][0]
+    for (let j = 0 ; j < (amount); j++) {
+
+        Letters.push(Apparitions[i][1])
+
+    }
+}
+
+console.log(Letters.length + Letters)
